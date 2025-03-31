4d9e6819fb5f681c16164cd667d12d4465df3a95
neocti <daphnefernandez000@gmail.com>
2025-04-01 02:03:19 +0800

    Densenet

--- lib/util/detection/rice_mobnet_detector.dart
+++ lib/util/detection/rice_mobnet_detector.dart
@@ -14,12 +14,12 @@ class RiceMobnetDetector {
     //    'assets/models/rice_model/rice_inception_model.tflite');
    //  _interpreter = await Interpreter.fromAsset(
    //      'assets/models/rice_model/rice_cnn_model.tflite');
-   //  _interpreter = await Interpreter.fromAsset(
-    //     'assets/models/rice_model/rice_dense_net_model.tflite');
+     _interpreter = await Interpreter.fromAsset(
+         'assets/models/rice_model/rice_dense_net_model.tflite');
     // _interpreter = await Interpreter.fromAsset(
     //     'assets/models/rice_model/rice_vgg_model.tflite');
-     _interpreter = await Interpreter.fromAsset(
-        'assets/models/rice_model/rice_Xception_model.tflite');
+    // _interpreter = await Interpreter.fromAsset(
+      //  'assets/models/rice_model/rice_Xception_model.tflite');
 
     final labelsData =
         await rootBundle.loadString('assets/models/rice_model/rice_label.txt');
